0fb9ae6fbe816233a3931b6163f360416ddb902d
Vandeth Nop <vnop14@gmail.com>
2017-06-27 00:42:42 -0700

    Recursion intro problems - WIP
    First two questions complete.

--- recursion/recursionIntro.js
+++ recursion/recursionIntro.js
@@ -1,7 +1,24 @@
 //1. Write a function that loops through the numbers n down to 0. If you haven't done so try using a while loop to do this.
-
+var loopIt = function(n) {
+  for (let i = n; i >= 0; i--) {
+    console.log(i);
+  }
+  return;
+};
+var loopWhile = function(n) {
+  while (n >= 0) {
+    n--;
+  }
+  return;
+};
 //2. Next, try looping just like above except using recursion
-
+var loopItRecursion = function(n) {
+  if (n === 0) {
+    return 'done';
+  } else {
+    return loopItRecursion(n - 1);
+  }
+};
 //3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
 
 //4. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
