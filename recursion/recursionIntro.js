cd00ffb57adabf439b2be95fdfcfa82e1b6e95e7
Vandeth Nop <vnop14@gmail.com>
2017-06-27 17:57:24 -0700

    Recursion intro exercise complete

--- recursion/recursionIntro.js
+++ recursion/recursionIntro.js
@@ -20,9 +20,38 @@ var loopItRecursion = function(n) {
   }
 };
 //3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
-
+  // Assuming they meant to say, return evaluation of the expression
+var exponent = function(base, expo) {
+  let result = base;
+  while (expo > 1) {
+    result *= base;
+    expo--;
+  }
+  return result;
+};
 //4. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
-
+var recursiveExponent = function(base, expo) {
+  if (expo === 1) {
+    return base;
+  } else {
+    return base * recursiveExponent(base, --expo);
+  }
+};
 //5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value into by num and returns an array of the values.
-
+var recursiveMultiplier = function(arr, num, position = 0) {
+  if (position === arr.length) {
+    return arr;
+  } else {
+    arr[position] = arr[position] * num;
+    return recursiveMultiplier(arr, num, ++position);
+  }
+};
 //6. Write a function 'recursiveReverse' that takes an array and uses recursion to return its contents in reverse
+var recursiveReverse = function(array, result = [], position = 0) {
+  if (position === array.length) {
+    return result;
+  } else {
+    result.unshift(array[position]);
+    return recursiveReverse(array, result, ++position);
+  }
+};
