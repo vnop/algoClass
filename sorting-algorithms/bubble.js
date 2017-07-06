5e45b4a263b7c61ed63c595cbf03082f67da659c
Vandeth Nop <vnop14@gmail.com>
2017-07-06 11:48:38 -0700

    Bubble sort complete

--- sorting-algorithms/bubble.js
+++ sorting-algorithms/bubble.js
@@ -19,3 +19,17 @@ Variants:
 (https://en.wikipedia.org/wiki/Cocktail_sort)
 
 */
+
+var bubbleSort = function(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i; j < array.length; j++) {
+      if (array[j] < array[i]) {
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+  return array;
+};
+// Time complexity: O(n^2)
